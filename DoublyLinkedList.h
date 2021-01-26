a8153903a6949828637f90078b853929991d5d70
ntrnl <ntrnlpssn@gmail.com>
2021-01-26 12:10:36 +0300

    add iterator and file_output/input

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -3,151 +3,510 @@
 
 #endif // DOUBLYLINKEDLIST_H
 
-#include <iostream>
-#include<QList>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+template <class T>
+class DoublyLinkedList;
+
+template <class Type>
+std::ostream& operator<<(std::ostream&, const DoublyLinkedList<Type>&);
+
+template <class Type>
+std::istream& operator>>(std::istream&, DoublyLinkedList<Type>&);
+
+template <class Type>
+std::wostream& operator<<(std::wostream&, const DoublyLinkedList<Type>&);
+
+template <class Type>
+std::wistream& operator>>(std::wistream&, DoublyLinkedList<Type>&);
+
+template <class T>
+std::wstring ToString(const DoublyLinkedList<T>& obj);
+
+
+template <class Type>
+bool operator==(const DoublyLinkedList<Type>& lha, const DoublyLinkedList<Type>& rha);
+
+template <class T>
+class Iterator;
+
+template<class T>
+class ListItem
+{
+public:
+    T value;
+private:
+    friend class DoublyLinkedList<T>;
+    friend class Iterator<T>;
+
+    friend std::ostream& operator<< <T> (std::ostream&, const DoublyLinkedList<T>&);
+    friend std::wostream& operator<< <T> (std::wostream&, const DoublyLinkedList<T>&);
+
+    friend std::istream& operator>> <T> (std::istream&, DoublyLinkedList<T>&);
+    friend std::wistream& operator>> <T> (std::wistream&, DoublyLinkedList<T>&);
+
+    friend bool operator== <T> (const DoublyLinkedList<T>& lha, const DoublyLinkedList<T>& rha);
+
+    explicit ListItem(const T& value);
+    ListItem* next;
+    ListItem* prev;
+};
+
+template <class T>
+ListItem<T>::ListItem(const T& value) : value(value), next(nullptr), prev(nullptr)
+{
+}
 
 template<class T>
 class DoublyLinkedList
 {
 public:
-    struct Node
-    {
-        T data;
-        Node* next;
-        Node* prev;
-        Node(const T& val): data(val), next(nullptr), prev(nullptr) {}
-    };
-    Node *head, *tail;
+    DoublyLinkedList();
+    DoublyLinkedList(const DoublyLinkedList<T>& list);
 
-      DoublyLinkedList(): head(nullptr), tail(nullptr) {}
+    DoublyLinkedList(std::initializer_list<T> other);
 
-     ~DoublyLinkedList()
-      {
-          Node *tmp = nullptr;
-          while (head)
-          {
-              tmp = head;
-              head = head->next;
-              delete tmp;
-          }
-          head = nullptr;
-      }
+    DoublyLinkedList<T>& operator= (const DoublyLinkedList<T>& other);
 
-      void addBegin(const T& val)
-      {
-          Node *node = new Node(val);
-        //  Node *tmp = head;
-          if (head == nullptr)
-          {
-              head = node;
-              tail = node;
-          }
-          else
-          {
-              node->next = head;
-              head = node;
-              node->next->prev = node;
-          }
-      }
+    friend std::ostream& operator<< <T> (std::ostream&, const DoublyLinkedList<T>&);
+    friend std::wostream& operator<< <T> (std::wostream&, const DoublyLinkedList<T>&);
 
-      void addEnd(const T& val)
-      {
-          Node *node = new Node(val);
-          if(tail == nullptr)
-          {
-              head = node;
-              tail = node;
-          }
-          else
-          {
-              tail->next = node;
-              node->prev = tail;
-              tail = node;
-          }
-      }
+    friend std::istream& operator>> <T> (std::istream&, DoublyLinkedList<T>&);
+    friend std::wistream& operator>> <T> (std::wistream&, DoublyLinkedList<T>&);
 
-      void deleteNode(Node *node)
-      {
-           Node *tmp = head;
+    friend bool operator== <T> (const DoublyLinkedList<T>& lha, const DoublyLinkedList<T>& rha);
 
-           while(tmp != node && tmp != nullptr)
-           {
-               tmp = tmp->next;
-           }
-           if(tmp == head)
-           {
-               head = head->next;
-               head->prev = nullptr;
-           }
-           else if(tmp == tail)
-           {
-               tail = tmp->prev;
-               tail->next = nullptr;
-           }
-           else
-           {
-               tmp->prev->next = tmp->next;
-               tmp->next->prev = tmp->prev;
-           }
-           delete tmp;
-       }
+    ~DoublyLinkedList();
 
-      void insertBefore(Node *node,const T& val)
-      {
-          if(node == head)
-          {
-              addBegin(val);
-              return;
-          }
+    void addBegin(const T& val);
+    void addEnd(const T& val);
+    void removeFirst();
+    void removeLast();
+    void deleteNode(ListItem<T> *node);
+    void insertBefore(ListItem<T> *node,const T& val);
+    void insertAfter(ListItem<T> *node, const T& val);
 
-          Node *newNode = new Node(val);
-          node->prev->next = newNode;
-          newNode->prev = node->prev;
-          node->prev = newNode;
-          newNode->next = node;
-      }
+    ListItem<T> *findVal(const T& val); //returns node of the given value
 
-      void insertAfter(Node *node, const T& val)
-      {
-          if(node == tail)
-          {
-              addEnd(val);
-              return;
-          }
+    Iterator<T> begin();
+    Iterator<T> end();
 
-          Node *newNode = new Node(val);
-          node->next->prev = newNode;
-          newNode->next = node->next;
-          node->next = newNode;
-          newNode->prev = node;
-      }
+    Iterator<const T> cbegin();
+    Iterator<const T> cend();
 
-      Node *findVal(const T& val) //returns node of the given number
-      {
-          Node *tmp = head;
-          while(tmp->data != val && tmp != nullptr)
-          {
-              tmp = tmp->next;
-          }
-          return tmp;
-       }
+    int count();
 
-      template <class U>
-      friend std::ostream & operator<<(std::ostream & os, const DoublyLinkedList<U> & dll){
-       dll.display(os);
-       return os;
-      }
+private:
 
-      private:
-
-    void display(std::ostream& out = std::cout) const
-    {
-         Node *node = head;
-         while(node != nullptr)
-         {
-             out << node->data << " ";
-             node = node->next;
-         }
-     }
+    ListItem<T> *head, *tail;
+    void destroy();
+    void copyElements(const DoublyLinkedList<T>&);
+    int _count;
 
 };
+
+
+template <class T>
+std::istream& operator>> (std::istream& in, DoublyLinkedList<T>& list)
+{
+    if (in.peek() == '\n' || in.eof())
+    {
+        return in;
+    }
+
+    T value;
+    while (in >> value)
+    {
+        list.addEnd(value);
+
+        if (in.peek() == '\n' || in.eof())
+        {
+            break;
+        }
+    }
+
+    return in;
+}
+
+template <class T>
+std::wistream& operator>> (std::wistream& in, DoublyLinkedList<T>& list)
+{
+    T value;
+    while (in >> value)
+    {
+        list.addEnd(value);
+        if (in.peek() == L'\n' || in.eof())
+        {
+            break;
+        }
+    }
+    return in;
+}
+
+template <class T>
+DoublyLinkedList<T>::DoublyLinkedList(): head(nullptr), tail(nullptr), _count(0)
+{
+}
+
+template <class T>
+DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList& list)
+    : DoublyLinkedList()
+{
+    this->copyElements(list);
+}
+
+template <class T>
+DoublyLinkedList<T>::DoublyLinkedList(std::initializer_list<T> other)
+    : DoublyLinkedList()
+{
+   std::for_each(other.begin(), other.end(), [this](const T item) { this->addEnd(item); });
+}
+
+template <class T>
+DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const DoublyLinkedList<T>& other)
+{
+    if (*this != other)
+    {
+        this->destroy();
+        this->copyElements(other);
+    }
+    return *this;
+}
+
+template <class T>
+bool operator==(DoublyLinkedList<T>& lha, DoublyLinkedList<T>& rha)
+{
+    if (lha.count() != rha.count()) { return false; }
+
+    return std::equal(lha.begin(), lha.end(), rha.begin());
+}
+
+template <class T>
+bool operator==(const DoublyLinkedList<T>& lha, const DoublyLinkedList<T>& rha)
+{
+    auto current_lha = lha.head;
+    auto current_rha = rha.head;
+    while (current_lha != nullptr && current_rha != nullptr)
+    {
+        if (current_lha->value != current_rha->value)
+        {
+            return false;
+        }
+
+        current_lha = current_lha->next;
+        current_rha = current_rha->next;
+    }
+    return current_lha == current_rha;
+}
+
+template <class T>
+std::ostream& operator<< (std::ostream& out, const DoublyLinkedList<T>& list)
+{
+    if (list.head == nullptr)
+    {
+        return out << " ";
+    }
+
+    //out << "{ ";
+    auto current = list.head;
+    while (current->next != nullptr)
+    {
+        out << current->value << " ";
+        current = current->next;
+    }
+    return out << current->value; // << " }";
+}
+
+template <class T>
+std::wostream& operator<< (std::wostream& out, const DoublyLinkedList<T>& list)
+{
+    if (list.IsEmpty())
+    {
+        return out << L" ";
+    }
+
+   // out << L"{ ";
+    auto current = list.head;
+    while (current->next != nullptr)
+    {
+        // TODO: проблема с wostream << string
+        out << current->value << L" ";
+        current = current->next;
+    }
+    // TODO: проблема с wostream << string
+    return out << current->value; // << L" }";
+}
+
+template <class T>
+std::wstring ToString(const DoublyLinkedList<T>& obj)
+{
+    std::wstringstream out;
+    out << obj;
+    return out.str();
+}
+
+template<class T>
+DoublyLinkedList<T>::~DoublyLinkedList()
+ {
+     ListItem<T> *tmp = nullptr;
+     while (head)
+     {
+         tmp = head;
+         head = head->next;
+         delete tmp;
+     }
+     head = nullptr;
+ }
+
+template<class T>
+void DoublyLinkedList<T>::addBegin(const T& val)
+ {
+     ListItem<T> *node = new ListItem<T>(val);
+     if (head == nullptr)
+     {
+         head = node;
+         tail = node;
+     }
+     else
+     {
+         node->next = head;
+         head = node;
+         node->next->prev = node;
+     }
+     _count++;
+ }
+
+template<class T>
+void DoublyLinkedList<T>::addEnd(const T& val)
+ {
+     ListItem<T> *node = new ListItem<T>(val);
+     if(tail == nullptr)
+     {
+         head = node;
+         tail = node;
+     }
+     else
+     {
+         tail->next = node;
+         node->prev = tail;
+         tail = node;
+     }
+     _count++;
+ }
+
+
+template<class T>
+void DoublyLinkedList<T>::removeFirst()
+{
+    this->head = this->head->next;
+    delete this->head->prev;
+    this->head->prev = nullptr;
+    _count--;
+}
+
+template<class T>
+void DoublyLinkedList<T>::removeLast()
+{
+    this->tail = this->tail->prev;
+    delete this->tail->next;
+    this->tail->next = nullptr;
+    _count--;
+}
+
+template<class T>
+void DoublyLinkedList<T>::deleteNode(ListItem<T> *node)
+ {
+      ListItem<T> *tmp = head;
+
+      while(tmp != node && tmp != nullptr)
+      {
+          tmp = tmp->next;
+      }
+      if(tmp == head)
+      {
+          head = head->next;
+          head->prev = nullptr;
+      }
+      else if(tmp == tail)
+      {
+          tail = tmp->prev;
+          tail->next = nullptr;
+      }
+      else
+      {
+          tmp->prev->next = tmp->next;
+          tmp->next->prev = tmp->prev;
+      }
+      delete tmp;
+      _count--;
+  }
+
+template<class T>
+void DoublyLinkedList<T>::insertBefore(ListItem<T> *node,const T& val)
+ {
+     if(node == head)
+     {
+         addBegin(val);
+         return;
+     }
+
+     ListItem<T> *newNode = new ListItem<T>(val);
+     node->prev->next = newNode;
+     newNode->prev = node->prev;
+     node->prev = newNode;
+     newNode->next = node;
+     _count++;
+ }
+
+template<class T>
+void DoublyLinkedList<T>::insertAfter(ListItem<T> *node, const T& val)
+ {
+     if(node == tail)
+     {
+         addEnd(val);
+         return;
+     }
+
+     ListItem<T> *newNode = new ListItem<T>(val);
+     node->next->prev = newNode;
+     newNode->next = node->next;
+     node->next = newNode;
+     newNode->prev = node;
+     _count++;
+ }
+
+template<class T>
+ListItem<T> *DoublyLinkedList<T>::findVal(const T& val) //returns node of the given number
+ {
+     ListItem<T> *tmp = head;
+     while(tmp->value != val && tmp != nullptr)
+     {
+         tmp = tmp->next;
+     }
+     return tmp;
+  }
+
+template <class T>
+Iterator<T> DoublyLinkedList<T>::begin()
+{
+    return Iterator<T>(this->head);
+}
+
+template <class T>
+Iterator<T> DoublyLinkedList<T>::end()
+{
+    return Iterator<T>(this->tail);
+}
+
+template <class T>
+Iterator<const T> DoublyLinkedList<T>::cbegin()
+{
+    return Iterator<const T>(reinterpret_cast<ListItem<const T>*>(this->head));
+}
+
+
+template <class T>
+Iterator<const T> DoublyLinkedList<T>::cend()
+{
+    return Iterator<const T>(reinterpret_cast<ListItem<const T>*>(this->tail));
+}
+
+template <class T>
+int DoublyLinkedList<T>::count()
+{
+    return _count;
+}
+
+template <class T>
+void DoublyLinkedList<T>::destroy()
+{
+    while(this->head != nullptr)
+    {
+        this->removeFirst();
+    }
+}
+
+template <class T>
+void DoublyLinkedList<T>::copyElements(const DoublyLinkedList<T>& other)
+{
+    auto current = other.head;
+    while(current != nullptr)
+    {
+        this->addBegin(current->value);
+        current = current->next;
+    }
+}
+
+
+
+template <class T>
+class Iterator
+{
+public:
+    explicit Iterator(ListItem<T>* item);
+    Iterator(const Iterator<T>& other);
+
+    bool operator == (Iterator<T> const& other) const;
+    bool operator != (Iterator<T> const& other) const;
+    Iterator<T> operator++();
+    Iterator<T> operator--();
+    T& operator *();
+    T& operator *() const;
+
+    friend class DoublyLinkedList<T>;
+
+    ListItem<T>* current;
+};
+
+template <class T>
+Iterator<T>::Iterator(ListItem<T>* item)
+{
+    this->current = item;
+}
+
+template<class T>
+Iterator<T>::Iterator(const Iterator<T> & other)
+{
+    this->current = other.current;
+}
+
+template <class T>
+bool Iterator<T>::operator==(Iterator<T> const& other) const
+{
+    return (this->current == other.current);
+}
+
+template <class T>
+bool Iterator<T>::operator!=(Iterator<T> const& other) const
+{
+    return (this->current != other.current);
+}
+
+template <class T>
+Iterator<T> Iterator<T>::operator++()
+{
+    this->current = this->current->next;
+    return Iterator<T>(this->current);
+}
+
+template <class T>
+Iterator<T> Iterator<T>::operator--()
+{
+    this->current = this->current->prev;
+    return Iterator<T>(this->current);
+}
+
+template <class T>
+T& Iterator<T>::operator*()
+{
+    return this->current->value;
+}
+
+template <class T>
+T& Iterator<T>::operator*() const
+{
+    return this->current->value;
+}
--- main.cpp
+++ main.cpp
@@ -1,35 +1,96 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <fstream>
 
 #include "DoublyLinkedList.h"
 
 using namespace std;
+
+void file_output_commented();
+int file_input(const string& filename);
+
+template <typename T>
+int file_output(const string& filename, const DoublyLinkedList<T>& data);
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     DoublyLinkedList<int> l1;
-      l1.addBegin(3);
-      l1.addEnd(5);
-      DoublyLinkedList<int>::Node *someNode = l1.tail;
-      l1.addEnd(12);
-      l1.addBegin(6);
-      l1.addEnd(88);
-      cout << l1 <<"\n";
-      l1.insertBefore(someNode, 77);
-      l1.insertAfter(someNode, 90);
-      someNode->data = 1488;
-      cout<<l1<<"\n";
-      l1.deleteNode(someNode);
-      cout<<l1<<"\n";
-      auto anotherNode = l1.findVal(6);
-      anotherNode->data = 99;
-      cout<<l1<<"\n";
-      l1.deleteNode(anotherNode);
-      cout<<l1<<"\n";
+    l1.addBegin(3);
+    l1.addEnd(5);
+    auto someNode = l1.end().current;
+    l1.addEnd(12);
+    l1.addBegin(6);
+    l1.addEnd(88);
+    cout << l1 <<"\n";
+    l1.insertBefore(someNode, 77);
+    l1.insertAfter(someNode, 90);
+    someNode->value = 1488;
+    cout<<l1<<"\n";
+    l1.deleteNode(someNode);
+    cout<<l1<<"\n";
+    auto anotherNode = l1.findVal(6);
+    anotherNode->value = 99;
+    cout<<l1<<"\n";
+    l1.deleteNode(anotherNode);
+    cout<<l1<<"\n";
+    file_output("list1.txt",l1);
+    file_input("list1.txt");
+    file_output_commented();
+    file_input("result.txt");
 
-     system("PAUSE");
+    system("PAUSE");
 
     return a.exec();
+}
+
+template <typename T>
+int file_output(const string& filename, const DoublyLinkedList<T>& data)
+{
+    ofstream out_file(filename.c_str(), ios_base::out);
+
+    if (!out_file.is_open())
+    {
+        cerr << "Error! Unable to open output file: " << filename << endl;
+        // C-style!
+        return -1;
+    }
+
+    out_file << data << endl;
+    out_file.close();
+
+    return 0;
+}
+
+int file_input(const string& filename)
+{
+    DoublyLinkedList<int> tmp;
+
+    ifstream in_file(filename, ios_base::in);
+
+    if (!in_file.is_open())
+    {
+        cerr << "Error! Unable to open output file: " << filename << endl;
+        // C-style!
+        return -1;
+    }
+
+    in_file >> tmp;
+
+    in_file.close();
+
+    cout << "list = " << tmp << ";" << endl;
+    return 0;
+}
+
+void file_output_commented()
+{
+    const DoublyLinkedList<int> tmp = { 1, 2, 3, 4, 5 };
+    cout << tmp << endl;
+
+    const auto code = file_output("result.txt", tmp);
+    cout << "code = " << code << endl;
+
+    cout << "END!" << endl;
 }
